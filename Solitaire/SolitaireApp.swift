a7a3e3d9f2f85a262fcb55a9928cb5c6b1e0064e
s3927562 <s3927562@rmit.edu.vn>
2023-08-23 19:03:18 +0700

    Initial Commit

---
+++ Solitaire/SolitaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolitaireApp.swift
+//  Solitaire
+//
+//  Created by Tung Tran Thanh on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SolitaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
